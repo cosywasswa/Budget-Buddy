5949814b4017cf6af6c07e279a6361d7cfbde53f
Cosmas Wasswa <cosywas@gmail.com>
2023-10-15 17:14:17 +0300

    Update categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-    include ImageUploader::Attachment(:icon)
   
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     has_many :payment_categories, dependent: :destroy
