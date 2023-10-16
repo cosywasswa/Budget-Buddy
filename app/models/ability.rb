5fe26820a250276603e691f568a225fc64cddb5e
Cosmas Wasswa <cosywas@gmail.com>
2023-10-16 14:18:34 +0300

    Installed and configured cancan for authorizing user to have control over the budget he owns

---
+++ app/models/ability.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+    can :manage, Category, author_id: user.id
+    can :manage, Payment, author_id: user.id
+  end
+end
