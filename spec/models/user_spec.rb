c6d238f5d9bd42b950ec5afaf81c48cd547f943f
Cosmas Wasswa <cosywas@gmail.com>
2023-10-16 17:07:32 +0300

    Add model tests for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+    RSpec.describe User, type: :Model do
+        let(:user) { User.new(name: 'Cosmas', email: 'cosywas@gmail.com', password: 'Cosy1234', confirmed_at: Time.now) }
+        before { user.save }
+      
+        it 'name should be present' do
+          user.name = nil
+          expect(user).to_not be_valid
+        end
+
+        it 'name should be 2 characters and above' do
+            user.name = "c"
+            expect(user).to_not be_valid
+          end
+
+          it 'name should be 40 characters and less' do
+            user.name = "co"*50
+            expect(user).to_not be_valid
+          end
+    end
